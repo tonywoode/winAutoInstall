850156b78f512a604d133baabe69b832f52a1110
tony woode <tonywoodecode@gmail.com>
2016-07-12 11:00:07 +0100

    added drive mapper for games on synology nas
    * If on the local network maps locally
    * If out of house, maps using netdrive and webdav
    
    The reason we map the nas first and then games is substed from it
    is that netdrive only allows one connection in the free version,
    I'll find out in 8 days if that works correctly

--- SetupVirtualDrives/mapCode/SubstCodeOnMac.bat
+++ SetupVirtualDrives/mapCode/SubstCodeOnMac.bat
@@ -1,3 +1,4 @@
+echo off & setlocal
 :: makes sure we have a drive O: that maps to code on osx
 :: needs to be run every startup
 :: problem is it lives on o: ;-)
@@ -18,3 +19,28 @@ if exist E:\CODE (subst O: E:\CODE)
 if exist E:\Users\twoode\CODE (subst O: E:\Users\twoode\CODE)
 if exist "\\Mac\Macintosh HD\Users\twoode\CODE" (net use O: "\\Mac\Macintosh HD\Users\twoode\CODE") || echo "can't map the network code drive"
 
+::now do the similar for mapping NAS box. If we're at home, map it on local network. If we're not, map it over webdav
+::using netdrive 2's command line tool. http://netdrive.net/ - I think this is going to be around for a while!!
+::netdrive adds its cmd to %PATH% automatically
+net use O: /D
+if
+if exist "\\Estuary\Games" (
+	net use N: \\Estuary\Games
+) else (
+	if exist "C:\Program Files\NetDrive2" (
+		:: you're going to need a config file for the secrets. Its going to need to live on O here
+		:: and note we assume that O mapped ok...
+		if not exist O:\Scripts\WinAutoInstall\SetupVirtualDrives\netDriveCreds.cfg ( echo "no credentials file, so cant map netdrive" && pause )
+		set config="O:\Scripts\WinAutoInstall\SetupVirtualDrives\netDriveCreds.cfg"
+		for /f "tokens=2* delims==" %%H in ('find "netDriveURL=" ^<%config% ') do (set URL=%%H)
+		for /f "tokens=2* delims==" %%I in ('find "netDriveUSER=" ^<%config% ') do (set USER=%%I)
+		for /f "tokens=2* delims==" %%J in ('find "netDrivePASS=" ^<%config% ') do (set PASS=%%J)
+		for /f "tokens=2* delims==" %%K in ('find "netDriveLETTER=" ^<%config% ') do (set LETTER=%%K)
+		( C:\Program Files\NetDrive2\nd2cmd -c m -t dav -u %URL% -a %USER% -p %PASS% -d %LETTER% -l nas 
+			subst N: "L:\GAMES") || (echo "can't map netdrive" && pause)
+	)
+)
+::clear up
+FOR %%Z IN (URL USER PASS LETTER) DO SET %%Z=
+exit
+	
