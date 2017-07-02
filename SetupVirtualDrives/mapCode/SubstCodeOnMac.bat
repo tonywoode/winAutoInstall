7252c10e9c8be403beb892533762841216f0fcac
tony woode <tonywoodecode@gmail.com>
2017-07-02 21:09:04 +0100

    kill subst, introuduce better logic anf F map

--- SetupVirtualDrives/mapCode/SubstCodeOnMac.bat
+++ SetupVirtualDrives/mapCode/SubstCodeOnMac.bat
@@ -7,7 +7,6 @@
 
 :: remove any previous mappings (though there shouldn't be any as nothing here is perm)
 net use O: /D
-subst /D O:
 
 ::three cases are 1)river 2)native boot on pond and 3) parallels boot on pond
 :: The last case is interesting, we CAN actually subst a network drive we map 
@@ -15,16 +14,16 @@ subst /D O:
 ::  resulting drive kicks off windows smartscreen every time. So just make
 ::  the Mac drive available on the parallels 'network' and we net use it instead.
 
-if exist E:\CODE (subst O: E:\CODE) else (
-	if exist E:\Users\twoode\CODE (subst O: E:\Users\twoode\CODE) else (
+if exist E:\CODE (net use O: \\localhost\E$\CODE) else (
+	if exist E:\Users\twoode\CODE (net use O: \\localhost\E$\Users\twoode\CODE) else (
 		if exist "\\Mac\Macintosh HD\Users\twoode\CODE" (net use O: "\\Mac\Macintosh HD\Users\twoode\CODE") || echo "can't map the network code drive"
 	)	
 )
 
 ::Map games drive in the same way. Outer loop is for river
 if not exist F:\GAMES_DRIVE (
-  if exist R:\GAMES_DRIVE (subst F: R:\) else (
-  	if exist S:\GAMES_DRIVE (subst F: S:\)
+  if exist R:\GAMES_DRIVE (net use F: \\localhost\R$) else (
+  	if exist S:\GAMES_DRIVE (net use F: \\localhost\S$)
   ) || echo "can't find games drive"
 )|| echo "games drive appears to be natively mapped"
 
@@ -34,17 +33,19 @@ if not exist F:\GAMES_DRIVE (
 net use N: /D
 
 ::if we're local, act local
-if exist "\\Estuary\Games" (net use N: \\Estuary\Games && EXIT /b)
-	
-::else mount netdrive		
-set config="O:\Scripts\WinAutoInstall\SetupVirtualDrives\netDriveCreds.cfg"
-for /f "tokens=2* delims==" %%H in ('find "netDriveURL=" ^<%config% ') do (set URL=%%H)
-for /f "tokens=2* delims==" %%I in ('find "netDriveUSER=" ^<%config% ') do (set USER=%%I)
-for /f "tokens=2* delims==" %%J in ('find "netDrivePASS=" ^<%config% ') do (set PASS=%%J)
-for /f "tokens=2* delims==" %%K in ('find "netDriveLETTER=" ^<%config% ') do (set LETTER=%%K)
-"C:\Program Files\NetDrive2\nd2cmd" -c m -t dav -u %URL% -a %USER% -p %PASS% -d %LETTER% -l nas 
-subst N: "L:\GAMES"
+if exist "\\Estuary\Games" (net use N: \\Estuary\Games && EXIT /b) else (
+ 
+  ::else mount netdrive		
+  set config="O:\Scripts\WinAutoInstall\SetupVirtualDrives\netDriveCreds.cfg"
+  for /f "tokens=2* delims==" %%H in ('find "netDriveURL=" ^<%config% ') do (set URL=%%H)
+  for /f "tokens=2* delims==" %%I in ('find "netDriveUSER=" ^<%config% ') do (set USER=%%I)
+  for /f "tokens=2* delims==" %%J in ('find "netDrivePASS=" ^<%config% ') do (set PASS=%%J)
+  for /f "tokens=2* delims==" %%K in ('find "netDriveLETTER=" ^<%config% ') do (set LETTER=%%K)
+  "C:\Program Files\NetDrive2\nd2cmd" -c m -t dav -u %URL% -a %USER% -p %PASS% -d %LETTER% -l nas 
+  net use N: \\localhost\L$\GAMES
 
+)
+ 
 ::clear up
 FOR %%Z IN (URL USER PASS LETTER) DO SET %%Z=
 ::exit
