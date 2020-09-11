598aa135e591346afc23af867a480da79d2c55ca
tony woode <tonywoodecode@gmail.com>
2020-09-11 21:13:30 +0100

    [choco] name choco folder...
    note that powershell policy is set to bypass for this script so you can
    run this whenever you want, but its position is contingent on the dots
    script, since it helps mentally if git is installed by choco before you
    start moving git config files around

---
+++ 4.Choco/ChocsAway_SHORTCUTTODESKTOP.bat
@@ -0,0 +1,23 @@
+@echo off & SETLOCAL
+
+::cd to this scripts dir
+cd /D "%~dp0"
+
+color 17
+::WE NEED AN EDITOR, AND WE NEED TO KNOW WHERE THE SCRIPTS ARE
+SET NOTEPADPLUSPLUS="C:\Program Files\Notepad++\notepad++.exe"
+
+ECHO.********************************************
+ECHO.		CHOCOLATEY TOOL
+ECHO.	QUERY FOR PACKAGES AND MAKE CHANGES FIRST
+ECHO.	THEN AFTER WE'LL RUN AND UPDATE
+ECHO.********************************************
+
+::we ALWAYS want to query choco first, so give us a command prompt so we can
+Start /B CMD /K
+
+IF EXIST %NOTEPADPLUSPLUS% (%NOTEPADPLUSPLUS% packages.config)
+
+::CHOCO LIKES TO RUN AS ADMIN
+.\..\Elevation\elevate installandUpdate
+pause
---
+++ 4.Choco/installandUpdate.cmd
@@ -0,0 +1,30 @@
+@echo off & SETLOCAL
+
+:: Installs chocolatey packages from a list, then updates all your installs, then reports back
+
+::cd to this scripts dir
+cd /D "%~dp0"
+::install from the xml
+cinst -y packages.config
+echo.
+echo.Now let's check for updates....
+echo.
+cup all -y
+
+echo.
+echo.Lastly, just to help out, I'll open that log
+echo.
+pause
+NOTEPAD C:\ProgramData\chocolatey\logs\chocolatey.log
+exit
+
+::unfortunately chocolatey doesn't let you use the packages.config for updates
+::hence having to update "all"
+::a method to update packages as it goes down a txt list of strings is below
+::but it would be a shame to have to go there
+::n.b.: the eol=# ensures we can put comments in a text file
+::
+::for /f "eol=# tokens=* delims=" %%g in (MyPrograms.txt) do (
+::	cinst -y %%g
+::	cup -y %%g
+::)
