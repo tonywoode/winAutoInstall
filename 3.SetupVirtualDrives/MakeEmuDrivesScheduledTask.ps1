dd2341fc9994ae9949195d912441c1d9e2e94308
tony woode <tonywoodecode@gmail.com>
2020-09-14 22:17:44 +0100

    [drive mappings] powershell task creation and run...
    the previous schtasks process for task creation is fatally flawed for
    some tasks, since by default all tasks created will unavoidably not run if the
    machine is on batteries:
    https://stackoverflow.com/questions/9075564/change-settings-for-power-for-windows-scheduled-task
    
    this is very unfortunate, however the powershell api for task creation
    is very good. By comparing the results of the previous manually-tweaked task xml
    export with the xml export of the powershell-created task, I was able to
    reproduce the required task programatically
    
    also cleared up the logic for naming drives, by causing the scheduled
    task to run as soon as created, at which point the initialise script can
    perform the first-time-only naming process
    
    doc still todo

---
+++ 3.SetupVirtualDrives/MakeEmuDrivesScheduledTask.ps1
@@ -0,0 +1,8 @@
+$Sta = New-ScheduledTaskAction -Execute "C:\Emulators\WinScripts\winAutoInstall\3.SetupVirtualDrives\mapDrives\mapEmuDrives.bat"
+$Stt = New-ScheduledTaskTrigger -AtLogon
+$NTAuthoritySystemUser = "S-1-5-18"
+$STPrin = New-ScheduledTaskPrincipal -UserId $NTAuthoritySystemUser -RunLevel "Highest"
+$Stset = New-ScheduledTaskSettingsSet -MultipleInstances "IgnoreNew" -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries 
+$TaskName = "MapEmuDrives"
+Register-ScheduledTask $TaskName -Action $Sta  -Trigger $Stt  -Principal $STPrin -Settings $Stset
+Start-ScheduledTask -TaskName $TaskName
