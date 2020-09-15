c36ef12951c2f2bdbb6998da6e37d6c5241813f0
tony woode <tonywoodecode@gmail.com>
2020-09-15 22:24:28 +0100

    [drive mappings] NAS mapper symlinks to desktop...
    as well as to startup folder
    ideally the logic would be able to be split over multiple lines, and
    ideally this would be a shortcut not a symlink, however you can't create
    shortcuts via cmd, and the powershell to create them is overly complex
    
    this imp at least prints messages and tries both actions correctly

--- 3.SetupVirtualDrives/2.linkNASMapperToStartup.bat
+++
@@ -1,4 +0,0 @@
-SET DIR=%~dp0%
-SET STARTUP=C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\
-..\Elevation\elevate cmd /c^
- "mklink "%STARTUP%mapNASDrives.bat" "%DIR%mapDrives\mapNASDrives.bat" && echo.copied NAS mapper to all users startup: start+r then 'shell:common startup' to get there & pause"
---
+++ 3.SetupVirtualDrives/2.linkNASMapperToStartupAndDesktop.bat
@@ -0,0 +1,5 @@
+SET DIR=%~dp0%
+SET STARTUP=C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\
+SET DESKTOP=%USERPROFILE%\Desktop\
+..\Elevation\elevate cmd /c^
+ "mklink "%STARTUP%mapNASDrives.bat" "%DIR%mapDrives\mapNASDrives.bat" & mklink "%DESKTOP%mapNASDrives.bat" "%DIR%mapDrives\mapNASDrives.bat" && echo.copied NAS mapper to Desktop & pause"
