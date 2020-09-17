14c35fd9a866b7f0263af3f4d47e650621390541
tony woode <tonywoodecode@gmail.com>
2020-09-17 15:50:21 +0100

    [drive mappings] minor improvements to emu mapper

--- 3.SetupVirtualDrives/mapDrives/mapNASDrives.bat
+++ 3.SetupVirtualDrives/mapDrives/mapNASDrives.bat
@@ -1,4 +1,4 @@
-@echo off & setlocal
+@echo OFF & SETLOCAL
 COLOR 17
 MODE CON: COLS=50 LINES=8
 
@@ -7,13 +7,7 @@ cd /D "%~dp0"
 ::for mapping NAS box. If we're at home, map it on local network. If we're not, map it over webdav
 ::using netdrive 2's command line tool. http://netdrive.net/ - I think this is going to be around for a while!!
 ::netdrive adds its cmd to %PATH% automatically
-if exist N:\ (
-  net use N: /D
-)
 
-if exist Q:\ (
-  net use Q: /D
-)
 
 :: get netdrive vars
 set config="..\netDriveCreds.cfg"
@@ -22,26 +16,33 @@ for /f "tokens=2* delims==" %%I in ('find "netDriveUSER=" ^<%config% ') do (set 
 for /f "tokens=2* delims==" %%J in ('find "netDrivePASS=" ^<%config% ') do (set PASS=%%J)
 for /f "tokens=2* delims==" %%K in ('find "netDriveLETTER=" ^<%config% ') do (set LETTER=%%K)
 
+Set NASDRIVELETTER=%LETTER:"=%:\
+Set NETDRIVECMD="C:\Program Files\NetDrive2\nd2cmd"
+
 :: this runs at startup, if you login too early, the local mapping doesn't work
-timeout 5 > NUL
+:: let's try without this for a while, just to check its needed in all circumstances
+::timeout 5 > NUL
 
 ::if we're local, act local. But note this check won't work unless you authenticate manually once and tick remember creds...
+if exist N:\ ( net use N: /D )
 net use N: \\Estuary\Emulators /user:%user% %PASS%
 ::else mount netdrive	
-if %errorlevel% NEQ 0 (
-  "C:\Program Files\NetDrive2\nd2cmd" -c m -t dav -u %URL% -a %USER% -p %PASS% -d %LETTER% -l nas 
-  subst N: L:\Emulators
+if %ERRORLEVEL% NEQ 0 (
+  %NETDRIVECMD% -c m -t dav -u %URL% -a %USER% -p %PASS% -d %LETTER% -l nas 
+  subst N: %NASDRIVELETTER%Emulators
 )
 
-::if we're local, act local. But note this check won't work unless you authenticate manually once and tick remember creds...
+if exist Q:\ ( net use Q: /D )
 net use Q: \\Estuary\Games /user:%user% %PASS%
 ::else mount netdrive	
-if %errorlevel% NEQ 0 (
-  "C:\Program Files\NetDrive2\nd2cmd" -c m -t dav -u %URL% -a %USER% -p %PASS% -d %LETTER% -l nas 
-  subst Q: L:\GAMES
+if %ERRORLEVEL% NEQ 0 (
+  if exist %NASDRIVELETTER% (
+    echo.NAS Drive already Mapped via NetDrive
+  ) else (
+    %NETDRIVECMD% -c m -t dav -u %URL% -a %USER% -p %PASS% -d %LETTER% -l nas 
+  )
+  subst Q: %NASDRIVELETTER%GAMES
 )
 
 ::clear up
 FOR %%Z IN (URL USER PASS LETTER) DO SET %%Z=
-::exit
-	
