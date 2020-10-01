4e9dc8af380d31dfe1b6442a6621e738568734b7
tony woode <tonywoodecode@gmail.com>
2020-10-01 23:48:52 +0100

    [choco] bandaid for chocs away script...
    2 basic problems:
    1) 'l' key doesn't work when symlink run ie: in notepad
    2) even powershell quierying should be done as admin
    (to avoid strange error messages eg: choco.exe.old cannot
    be accessed)
    We solve both by closing cmd window before notepad-ing
    and opening powershell as admin
    A better solution would probably be to use powershell entirely here

--- 4.Choco/ChocsAway.bat
+++ 4.Choco/ChocsAway.bat
@@ -11,12 +11,22 @@ ECHO.********************************************
 ECHO.		CHOCOLATEY TOOL
 ECHO.	QUERY FOR PACKAGES AND MAKE CHANGES FIRST
 ECHO.	THEN AFTER WE'LL RUN AND UPDATE
+ECHO.
+ECHO. THERE ARE TWO ADMIN PROMPTS NOW: ONE FOR POWERSHELL
+ECHO.  ANOTHER FOR RUNNING CHOCO. ONLY RUN THEE SECOND ONCE
+ECHO.  YOU'VE QUIERED
 ECHO.********************************************
 
-::we ALWAYS want to query choco first, so give us a command prompt so we can
-Start /B CMD /K
+:: We want an admin powershell window really (after problems with 'could not delete choco.exe.old'
+:: And we want to close the cmd window we just opened (after, I kid you not, problems with the 'l' key
+::  when this script is run via a symlink....
+:: https://code.adonline.id.au/use-a-batch-file-to-run-your-powershell-scripts/
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass' -Verb RunAs}"
 
-IF EXIST %NOTEPADPLUSPLUS% (%NOTEPADPLUSPLUS% packages.config)
+
+
+IF EXIST %NOTEPADPLUSPLUS% (START "" %NOTEPADPLUSPLUS% packages.config)
+
 
 ::CHOCO LIKES TO RUN AS ADMIN
 .\..\Elevation\elevate installandUpdate
